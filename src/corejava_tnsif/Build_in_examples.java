306852a3596ee6a46cf993139292bfa05b347a0e
Harshitha M <harshitha47m@gmail.com>
2025-08-13 20:13:42 +0530

    Files are added

---
+++ src/corejava_tnsif/Build_in_examples.java
@@ -0,0 +1,17 @@
+package corejava_tnsif;
+
+public class Build_in_examples {
+    public static void main(String[] args) {
+        String greeting="Hello Harshitha";
+        System.out.println("Original String -"+greeting);
+        System.out.println("Upper case -"+greeting.toUpperCase());
+        System.out.println("substring -"+greeting.substring(6));
+
+        int max=Math.max(10,20);
+        double sqrt=Math.sqrt(30.0);
+        System.out.println("max number "+ max);
+        System.out.println("sqrt "+sqrt);
+    }
+
+
+}
